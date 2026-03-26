567a7003c3886dbe2743598ea284d98fe3dac6e3
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2026-03-26 11:29:31 -0230

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { db } from "@benstack-aws/db";
+import * as schema from "@benstack-aws/db/schema/auth";
+import { env } from "@benstack-aws/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
